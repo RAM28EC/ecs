f55ce58669f831acc7df1557910f585ba07291be
RAM28EC <88874177+RAM28EC@users.noreply.github.com>
2022-07-18 22:16:36 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+ return "Welcome to Dummy Web Page"
+if __name__ == '__main__':
+ app.run(host="0.0.0.0", port=80)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
